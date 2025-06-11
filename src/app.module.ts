ce6dd56b754483e6269b29d7902a579302ff5d32
Carlos Junior <carlosjr260803@hotmail.com>
2025-06-11 14:06:07 -0500

    Change App module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+  TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: process.env.HOST,
+    username: process.env.USER,
+    password: process.env.PASSWORD+"",
+    database: process.env.DATABASE, 
+    port: 5432, 
+    entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    synchronize: true, // Set to false in production
+    retryAttempts: 3, // Retry connection attempts
+    retryDelay: 3000, // Delay between retries in milliseconds
+  }),
+  
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
