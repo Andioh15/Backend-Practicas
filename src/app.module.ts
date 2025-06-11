c243e6f96cf57dcd521d552e773534b87d0c4c3e
CappCat <abiudvera123@gmail.com>
2025-06-11 14:24:16 -0500

    updated appmodules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,22 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-  TypeOrmModule.forRoot({
-    type: 'postgres',
-    host: process.env.HOST,
-    username: process.env.USER,
-    password: process.env.PASSWORD+"",
-    database: process.env.DATABASE, 
-    port: 5432, 
-    entities: [__dirname + '/**/*.entity{.ts,.js}'],
-    synchronize: true, // Set to false in production
-    retryAttempts: 3, // Retry connection attempts
-    retryDelay: 3000, // Delay between retries in milliseconds
-  }),
-  
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.HOST,
+      username: process.env.USER,
+      password: process.env.PASSWORD,
+      database: process.env.DATABASE, 
+      port: Number(process.env.PORT), 
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true, // Set to false in production
+      retryAttempts: 3, // Retry connection attempts
+      retryDelay: 3000, // Delay between retries in milliseconds
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
